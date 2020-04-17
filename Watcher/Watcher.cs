6637a3f0695200a6ed1798d2994cdebece64874d
Nikolay Borodin <mr.kolyamba99@yandex.ru>
2020-04-17 13:35:24 +0400

    Создал класс Watcher

---
+++ Watcher/Watcher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Watcher
+{
+    public class Watcher
+    {
+        private readonly IMessageSender _sender;
+        private readonly IDirectoryWatcher _directoryWatcher;
+        private readonly Action _waitForQuit;
+
+        public Watcher(IMessageSender sender, Action waitForQuit, IDirectoryWatcher directoryWatcher)
+        {
+            _sender = sender ?? throw new ArgumentNullException(nameof(sender));
+            _waitForQuit = waitForQuit ?? throw new ArgumentNullException(nameof(waitForQuit));
+            _directoryWatcher = directoryWatcher ?? throw new ArgumentNullException(nameof(directoryWatcher));
+            _directoryWatcher.AddNewFileHandler(OnNewFile);
+        }
+
+        public void BeginWatch()
+        {
+            _directoryWatcher.BeginWatch();
+
+            _waitForQuit();
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e) => _sender.Send(e.FullPath);
+
+        private void OnNewFile(string fullPath)
+        {
+            if (!fullPath.EndsWith(".jpg") && !fullPath.EndsWith(".png") && !fullPath.EndsWith(".bmp")) return;
+            _sender.Send(fullPath);
+        }
+    }
+    
+}
