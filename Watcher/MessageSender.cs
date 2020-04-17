96dff29f2bdb43a583b435c20a2c8d20297e119c
Nikolay Borodin <mr.kolyamba99@yandex.ru>
2020-04-17 16:11:10 +0400

    реализовал MessageSender

---
+++ Watcher/MessageSender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using EasyNetQ;
+
+namespace Watcher
+{
+    public class MessageSender: IMessageSender, IDisposable
+    {
+        private readonly IBus _bus;
+
+        public MessageSender()
+            => _bus = RabbitHutch.CreateBus("host=localhost");
+
+        public void Send(string filename) 
+            => _bus.Publish(filename);
+
+        public void Dispose() 
+            => _bus.Dispose();
+    }
+}
