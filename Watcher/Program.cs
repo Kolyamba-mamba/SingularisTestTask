df6dbc0e4bf536ad937505209325d6988d1b05de
Nikolay Borodin <mr.kolyamba99@yandex.ru>
2020-04-17 16:12:59 +0400

    Реализовал работу приложения

--- Watcher/Program.cs
+++ Watcher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Watcher
 {
@@ -6,7 +7,22 @@ namespace Watcher
     {
         static void Main(string[] args)
         {
-            
+            const string incomingString = "incoming";
+            using var directoryWatcher = new DirectoryWatcher(incomingString); 
+            using var messageSender = new MessageSender();
+            var watcher = new Watcher(messageSender, Stop, directoryWatcher);
+            watcher.BeginWatch();
+        }
+
+        private static void Stop()
+        {
+            const string exitMessage = "quit";
+            while (true)
+            {
+                Console.WriteLine($"Press '{exitMessage}' to quit the sample.");
+                if (Console.ReadLine() == exitMessage)
+                    break;
+            }
         }
     }
 }
