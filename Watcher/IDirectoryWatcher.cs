21365fac1e450d8dd603befc9ab18d4bca0d6dc4
Nikolay Borodin <mr.kolyamba99@yandex.ru>
2020-04-17 13:32:47 +0400

    Создал интерфейс IDirectoryWatcher

---
+++ Watcher/IDirectoryWatcher.cs
@@ -0,0 +1,9 @@
+﻿namespace Watcher
+{
+    public interface IDirectoryWatcher
+    {
+        public delegate void NewFileDelegate(string fullPath);
+        void AddNewFileHandler(NewFileDelegate newFileDelegate);
+        void BeginWatch();
+    }
+}
