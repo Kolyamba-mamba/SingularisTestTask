8341067cc9aafb9fb670ffadf26b553711638b63
Nikolay Borodin <mr.kolyamba99@yandex.ru>
2020-04-18 17:22:28 +0400

    реализовал запуск конвертера

---
+++ Converter/CommandLineOptions.cs
@@ -0,0 +1,27 @@
+﻿using MatthiWare.CommandLine.Core.Attributes;
+
+namespace Converter
+{
+    public class CommandLineOptions
+    {
+        [Name("o", "opacity")]
+        [Description("Прозрачность водяного знака.")]
+        [DefaultValue(100)]
+        public int Opacity { get; set; } = 100;
+        
+        [Name("s", "relative-size")]
+        [Description("Размер водяного знака относительно изначальной картинки.")]
+        [DefaultValue(50)]
+        public int WatermarkRelativeSize { get; set; } = 50;
+        
+        [Name(null, "position")]
+        [Description("Позиция водяного знака на картинке.")]
+        [DefaultValue(WatermarkPosition.Center)]
+        public WatermarkPosition WatermarkPosition { get; set; } = WatermarkPosition.Center;
+
+        [Name(null, "path")]
+        [Description("Путь к водяному знаку")]
+        [DefaultValue("logo.png")]
+        public string WatermarkPath { get; set; } = "logo.png";
+    }
+}
--- Converter/Program.cs
+++ Converter/Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Drawing;
+using Common;
+using MatthiWare.CommandLine;
 
 namespace Converter
 {
@@ -6,7 +9,49 @@ namespace Converter
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string outDirectory = "outcoming";
+            using var messageReceiver = new MessageReceiver<BusMessage>();
+
+            var fileManager = new FileManager();
+            var directoryManager = new DirectoryManager(outDirectory);
+            var settings = ParseCommandLine(args);
+            if (settings is null)
+                return;
+            var imageConverter = new WatermarkOverlayer(settings?.logo,
+                new WatermarkSettings
+                {
+                    Opacity = (int) settings?.settings.Opacity, 
+                    Position = (WatermarkPosition) settings?.settings.Position, 
+                    WatermarkRelativeSize = (int) settings?.settings.WatermarkRelativeSize
+                });
+            var converter = new Converter(messageReceiver, imageConverter, fileManager, directoryManager);
+            Run();
+        }
+
+        private static void Run()
+        {
+            const string exitMessage = "quit";
+            while (true)
+            {
+                Console.WriteLine($"Press '{exitMessage}' to quit the sample.");
+                if (Console.ReadLine() == exitMessage)
+                    break;
+            }
+        }
+
+        private static (Image logo, WatermarkSettings settings)? ParseCommandLine(string[] args)
+        {
+            var parser = new CommandLineParser<CommandLineOptions>();
+            var parsingResult = parser.Parse(args);
+            if (parsingResult.HelpRequested)
+                return null;
+            var result = parsingResult.Result;
+            return (Image.FromFile(result.WatermarkPath), new WatermarkSettings
+            {
+                Opacity = result.Opacity,
+                WatermarkRelativeSize = result.WatermarkRelativeSize,
+                Position = result.WatermarkPosition
+            });
         }
     }
 }
